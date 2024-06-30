4be5ae3318fc1031fa84be5303273516e9776fe6
Suronjit797 <suronjit797@gmail.com>
2024-06-30 22:25:11 +0600

    question form format

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,15 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import axios from "axios";
 
 // use without lazy
 
 const queryClient = new QueryClient();
 // axios base url
-// axios.defaults.baseURL = "http://localhost:5000/api/v1"; 
-axios.defaults.baseURL = "https://question-server-jm21.onrender.com/api/v1"; 
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
+// axios.defaults.baseURL = "https://question-server-jm21.onrender.com/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/pages/Question/QuestionForm.jsx
+++ src/pages/Question/QuestionForm.jsx
@@ -1,19 +1,19 @@
 import { UploadOutlined } from "@ant-design/icons";
 import { useMutation } from "@tanstack/react-query";
-import { Button, Form, Input, Radio, Select, Upload } from "antd";
+import { Button, Form, Input, Select, Upload } from "antd";
 import axios from "axios";
 import { useState } from "react";
 const { Option } = Select;
 
-const initdata = {
+const initData = {
   type: "written",
   question: "asasd",
   options: ["a", "v", "b", "s", "d"],
   answerIndex: "3",
   answerText: "a",
   uploader: "667ff284c9191d4994ff7275",
-  subject: "667ff284c9191d4994ff7275",
-  paper: "667ff284c9191d4994ff7275",
+  subject: "math",
+  paper: "first",
   topics: "667ff284c9191d4994ff7275",
   tags: ["a"],
   institution: "a",
@@ -32,9 +32,8 @@ const createQuestion = (body) => {
 
 const QuestionForm = ({ mode = "create", data = {} }) => {
   const [form] = Form.useForm();
-  const [type, setType] = useState("");
-  const [sImage, setSImage] = useState("");
-  const [qImage, setQimage] = useState("");
+  const [formData, setFormData] = useState({});
+  const { type, solutionsImage, questionImage } = formData;
 
   const { mutate, isError, error } = useMutation({
     mutationKey: "createQuestion",
@@ -43,7 +42,37 @@ const QuestionForm = ({ mode = "create", data = {} }) => {
 
   const handleFinish = async (values) => {
     console.log("Form Values:", values);
-    
+
+    // return;
+    // const formData = new FormData();
+
+    // // Append simple fields to formData
+    // formData.append("type", values.type);
+    // formData.append("question[text]", values.question);
+    // formData.append("answerIndex", values.answerIndex);
+    // formData.append("answerText", values.answerText);
+    // formData.append("uploader", values.uploader);
+    // formData.append("subject", values.subject);
+    // formData.append("paper", values.paper);
+    // formData.append("topics", values.topics);
+    // formData.append("institution", values.institution);
+    // formData.append("year", values.year);
+    // formData.append("difficulty", values.difficulty);
+    // formData.append("options", values.options);
+    // formData.append("tags", values.tags);
+
+    // values.questionImage?.forEach((file, index) => {
+    //   formData.append(`questionImage`, file.originFileObj);
+    // });
+    // values.solutionsImage?.forEach((file, index) => {
+    //   formData.append(`solutionsImage`, file.originFileObj);
+    // });
+
+    // try {
+    //   mutate(formData);
+    // } catch (error) {
+    //   console.log(error);
+    // }
   };
 
   const normFile = (e) => {
@@ -55,179 +84,88 @@ const QuestionForm = ({ mode = "create", data = {} }) => {
 
   return (
     <div className="container p-11 my-auto">
-      <Form
-        form={form}
-        onFinish={handleFinish}
-        onFinishFailed={finishFailed}
-        layout="vertical"
-        initialValues={initdata}
-      >
-        <div className="grid grid-cols-2 gap-6">
-          <Form.Item
-            name="type"
-            label="Type"
-            rules={[
-              {
-                required: true,
-                message: "Please input the type",
-              },
-            ]}
-          >
-            <Select
-              placeholder="Add options"
-              onChange={(value) => setType(value)}
-              options={[
-                { label: "MCQ", value: "mcq" },
-                { label: "Written", value: "written" },
+      <div className="max-w-[450px] mx-auto">
+        <Form
+          form={form}
+          onFinish={handleFinish}
+          onFinishFailed={finishFailed}
+          layout="vertical"
+          initialValues={initData}
+          onValuesChange={(_, all) => setFormData(all)}
+        >
+          {/* <div className="grid grid-cols-2 gap-6"> */}
+          <div>
+            <Form.Item
+              name="type"
+              label="Type"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the type",
+                },
               ]}
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="question"
-            label="Question Text"
-            rules={[
-              { required: true, message: "Please input the question text" },
-            ]}
-          >
-            <Input placeholder="Enter Question Text" />
-          </Form.Item>
-
-          {type === "mcq" && (
-            <>
-              <Form.Item
-                name="options"
-                label="Options"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input the options",
-                    type: "array",
-                  },
+            >
+              <Select
+                placeholder="Add options"
+                options={[
+                  { label: "MCQ", value: "mcq" },
+                  { label: "Written", value: "written" },
                 ]}
-              >
-                <Select
-                  mode="tags"
-                  placeholder="Add options"
-                  maxCount="4"
-                ></Select>
-              </Form.Item>
-              <Form.Item
-                name="answerIndex"
-                label="Answer Index"
-                rules={[
-                  { required: true, message: "Please input the answer index" },
-                ]}
-              >
-                <Input type="number" />
-              </Form.Item>
-            </>
-          )}
+              />
+            </Form.Item>
 
-          <Form.Item
-            name="answerText"
-            label="Answer Text"
-            rules={[
-              { required: true, message: "Please input the answer text" },
-            ]}
-          >
-            <Input placeholder="Enter Answer Text" />
-          </Form.Item>
+            <Form.Item
+              name="question"
+              label="Question Text"
+              rules={[{ required: true, message: "Please input the question text" }]}
+            >
+              <Input placeholder="Enter Question Text" />
+            </Form.Item>
 
-          <Form.Item
-            name="uploader"
-            label="Uploader ID"
-            rules={[
-              { required: true, message: "Please input the uploader ID" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="subject"
-            label="Subject ID"
-            rules={[{ required: true, message: "Please input the subject ID" }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="paper"
-            label="Paper ID"
-            rules={[{ required: true, message: "Please input the paper ID" }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="topics"
-            label="Topics ID"
-            rules={[{ required: true, message: "Please input the topics ID" }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="tags"
-            label="Tags"
-            rules={[
-              {
-                required: true,
-                message: "Please input the tags",
-                type: "array",
-              },
-            ]}
-          >
-            <Select mode="tags" maxCount={5} placeholder="Add tags"></Select>
-          </Form.Item>
-
-          <Form.Item
-            name="institution"
-            label="Institution"
-            rules={[
-              { required: true, message: "Please input the institution" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item name="year" label="Year">
-            <Input type="number" />
-          </Form.Item>
-
-          <Form.Item
-            name="difficulty"
-            label="Difficulty"
-            rules={[
-              { required: true, message: "Please select the difficulty" },
-            ]}
-          >
-            <Select placeholder="Select difficulty">
-              <Option value="easy">Easy</Option>
-              <Option value="medium">Medium</Option>
-              <Option value="hard">Hard</Option>
-            </Select>
-          </Form.Item>
-
-          <div className="grid grid-cols-2 gap-6">
             <Form.Item
               name="questionImage"
               label="Question Image"
               valuePropName="fileList"
               getValueFromEvent={normFile}
             >
-              <Upload
-                name="questionImage"
-                listType="picture"
-                beforeUpload={() => false}
-                maxCount={5}
-                onChange={(value) => setQimage(value.fileList)}
-              >
-                <Button disabled={qImage.length >= 5} icon={<UploadOutlined />}>
+              <Upload name="questionImage" listType="picture" beforeUpload={() => false} maxCount={5}>
+                <Button disabled={questionImage?.length >= 5} icon={<UploadOutlined />}>
                   Upload
                 </Button>
               </Upload>
+            </Form.Item>
+
+            {type === "mcq" && (
+              <>
+                <Form.Item
+                  name="options"
+                  label="Options"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input the options",
+                      type: "array",
+                    },
+                  ]}
+                >
+                  <Select mode="tags" placeholder="Add options" maxCount="4" />
+                </Form.Item>
+                <Form.Item
+                  name="answerIndex"
+                  label="Answer Index"
+                  rules={[{ required: true, message: "Please input the answer index" }]}
+                >
+                  <Input type="number" />
+                </Form.Item>
+              </>
+            )}
+
+            <Form.Item
+              name="answerText"
+              label="Answer Text"
+              rules={[{ required: true, message: "Please input the answer text" }]}
+            >
+              <Input placeholder="Enter Answer Text" />
             </Form.Item>
 
             <Form.Item
@@ -236,26 +174,110 @@ const QuestionForm = ({ mode = "create", data = {} }) => {
               valuePropName="fileList"
               getValueFromEvent={normFile}
             >
-              <Upload
-                name="solutionsImage"
-                listType="picture"
-                beforeUpload={() => false}
-                maxCount={5}
-                onChange={(value) => setSImage(value.fileList)}
-              >
-                <Button disabled={sImage.length >= 5} icon={<UploadOutlined />}>
+              <Upload name="solutionsImage" listType="picture" beforeUpload={() => false} maxCount={5}>
+                <Button disabled={solutionsImage?.length >= 5} icon={<UploadOutlined />}>
                   Upload
                 </Button>
               </Upload>
             </Form.Item>
+
+            <Form.Item
+              name="uploader"
+              label="Uploader"
+              rules={[{ required: true, message: "Please input the uploader" }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="subject"
+              label="Subject"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the subject",
+                },
+              ]}
+            >
+              <Select
+                placeholder="Add Subject"
+                options={["physics", "chemistry", "math"].map((item) => ({
+                  label: <span className="capitalize">{item}</span>,
+                  value: item,
+                }))}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="paper"
+              label="Subject"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the paper",
+                },
+              ]}
+            >
+              <Select
+                placeholder="Add Paper"
+                options={["first", "second"].map((item) => ({
+                  label: <span className="capitalize">{item}</span>,
+                  value: item,
+                }))}
+              />
+            </Form.Item>
+
+            <Form.Item name="topics" label="Topics" rules={[{ required: true, message: "Please input the topics" }]}>
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="tags"
+              label="Tags"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the tags",
+                  type: "array",
+                },
+              ]}
+            >
+              <Select mode="tags" maxCount={5} placeholder="Add tags" />
+            </Form.Item>
+
+            <Form.Item
+              name="institution"
+              label="Institution"
+              rules={[{ required: true, message: "Please input the institution" }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item name="year" label="Year">
+              <Input type="number" />
+            </Form.Item>
+
+            <Form.Item
+              name="difficulty"
+              label="Difficulty"
+              rules={[{ required: true, message: "Please select the difficulty" }]}
+            >
+              <Select
+                placeholder="Select difficulty"
+                options={["easy", "medium", "hard"].map((item) => ({
+                  label: <span className="capitalize">{item}</span>,
+                  value: item,
+                }))}
+              />
+            </Form.Item>
           </div>
-        </div>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
