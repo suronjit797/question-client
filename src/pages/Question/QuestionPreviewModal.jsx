e12cf85308620b118356735c74529ee96f92cbc2
Suronjit797 <suronjit797@gmail.com>
2024-07-05 19:53:57 +0600

    update question

--- src/pages/Question/QuestionPreviewModal.jsx
+++ src/pages/Question/QuestionPreviewModal.jsx
@@ -1,7 +1,8 @@
 import { Modal, Tag } from "antd";
 import PropTypes from "prop-types";
-import { isValidUrl } from "../../utils/helpers";
 import PrintMath from "../../components/PrintMath/PrintMath";
+import { useMutation } from "@tanstack/react-query";
+import { createQuestionFn, updateQuestionFn } from "../../transtackQuery/questionApis";
 
 const optionNumber = {
   option1: "a",
@@ -10,7 +11,22 @@ const optionNumber = {
   option4: "d",
 };
 
-const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
+const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data, mode }) => {
+  // fetch
+  const { mutate, isError, error } = useMutation({
+    mutationKey: "createQuestion",
+    mutationFn: createQuestionFn,
+  });
+
+  const {
+    mutate: update,
+    // isError,
+    // error,
+  } = useMutation({
+    mutationKey: "updateQuestion",
+    mutationFn: updateQuestionFn,
+  });
+
   const handleClose = () => {
     setIsModalOpen(false);
   };
@@ -32,9 +48,89 @@ const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
     solution,
   } = data;
   const optionsItems = Object.keys(options);
+
+  const handleOk = () => {
+    console.log({ data });
+    const formData = new FormData();
+
+    formData.append("type", data.type);
+    formData.append("question[text]", data.question.text);
+    formData.append("solution[text]", data.solution.text);
+    data.question.images.forEach((image, index) => {
+      if (image.originFileObj) {
+        formData.append(`question.images`, image.originFileObj);
+      } else {
+        formData.append(`question[images][${index}][uid]`, image.uid);
+        formData.append(`question[images][${index}][name]`, image.name);
+        formData.append(`question[images][${index}][thumbUrl]`, image.thumbUrl);
+        formData.append(`question[images][${index}][size]`, image.size);
+        formData.append(`question[images][${index}][type]`, image.type);
+      }
+    });
+    data.solution.images.forEach((image, index) => {
+      if (image.originFileObj) {
+        formData.append(`solution.images`, image.originFileObj);
+      } else {
+        formData.append(`solution[images][${index}][uid]`, image.uid);
+        formData.append(`solution[images][${index}][name]`, image.name);
+        formData.append(`solution[images][${index}][thumbUrl]`, image.thumbUrl);
+        formData.append(`solution[images][${index}][size]`, image.size);
+        formData.append(`solution[images][${index}][type]`, image.type);
+      }
+    });
+    formData.append("answerIndex", data.answerIndex);
+    formData.append("answerText", data.answerText);
+    formData.append("uploader", data.uploader);
+    formData.append("subject", data.subject);
+    formData.append("paper", data.paper);
+    formData.append("chapter", data.chapter);
+    formData.append("topics", data.topics);
+    formData.append("optionType", data.optionType);
+    data.tags.forEach((tag, index) => {
+      formData.append(`tags[${index}]`, tag);
+    });
+    formData.append("institution", data.institution);
+    formData.append("year", data.year);
+    formData.append("difficulty", data.difficulty);
+    Object.keys(data.options).forEach((key) => {
+      console.log(data.options[key], key, data);
+      if (typeof data.options[key] === "string") {
+        formData.append(`options[${key}]`, data.options[key]);
+      } else if (data.options[key][0]?.originFileObj) {
+        formData.append(`options.${key}`, data.options[key][0].originFileObj);
+      } else {
+        formData.append(`options[${key}][uid]`, data.options[key].uid);
+        formData.append(`options[${key}][name]`, data.options[key].name);
+        formData.append(`options[${key}][thumbUrl]`, data.options[key].thumbUrl);
+        formData.append(`options[${key}][size]`, data.options[key].size);
+        formData.append(`options[${key}][type]`, data.options[key].type);
+      }
+    });
+    data.institutions.forEach((institution, index) => {
+      formData.append(`institutions[${index}][name]`, institution.name);
+      formData.append(`institutions[${index}][year]`, institution.year);
+    });
+
+    // Append fields to formData using map
+    // Object.entries(data).forEach(([key, value]) => {
+    //   if (Array.isArray(value)) {
+    //     value.forEach((item, index) => {
+    //       formData.append(`${key}[${index}]`, item);
+    //     });
+    //   } else {
+    //     formData.append(key, value);
+    //   }
+    // });
+
+    if (mode === "create") {
+      mutate(formData);
+    } else {
+      update({ id: data._id, body: formData });
+    }
+  };
   return (
     <>
-      <Modal title="Preview" open={isModalOpen} onOk={handleClose} onCancel={handleClose} centered className="my-4">
+      <Modal title="Preview" open={isModalOpen} onOk={handleOk} onCancel={handleClose} centered className="my-4">
         <div>
           {institutions?.map((ins, ind) => (
             <h3 key={ind} className="text-md font-semibold text-center">
@@ -57,7 +153,9 @@ const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
               <span className="font-bold">Type:</span> {type}
             </div>
           </div>
+
           <hr className="my-3" />
+
           <div className="mb-2">
             {/* question image */}
             <div className=" my-3">
@@ -76,12 +174,13 @@ const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
               </div>
             </div>
           </div>
+
           {type === "mcq" ? (
             <div className="grid grid-cols-2 gap-5">
               {Array.isArray(optionsItems) &&
                 optionsItems?.map((item, key) => (
                   <div key={key}>
-                    <div className="flex items-center gap-4">
+                    <div className="grid grid-flow-col items-center gap-4">
                       <div> {optionNumber[item]}. </div>
 
                       {typeof options[item] === "string" ? (
@@ -89,8 +188,6 @@ const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
                       ) : (
                         <img src={options[item]?.thumbUrl} alt={options[item]?.uid} />
                       )}
-                      {/* </div>
-                      )} */}
                     </div>
                   </div>
                 ))}
@@ -98,7 +195,9 @@ const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
           ) : (
             <></>
           )}
+
           <hr className="my-3" />
+
           <div>
             <span className="font-bold">Answer:</span>
             {type === "mcq" ? (
@@ -109,6 +208,7 @@ const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
               <span>{answerText}</span>
             )}
           </div>
+
           <div className="capitalize">
             <span className="font-bold">Difficulty:</span> {difficulty}
           </div>
@@ -121,7 +221,9 @@ const QuestionPreviewModal = ({ isModalOpen, setIsModalOpen, data }) => {
                 </Tag>
               ))}
           </div>
+
           <hr className="my-3" />
+
           <div>
             <h2 className="font-bold mb-2">Solutions:</h2>
             <h2 className=" mb-2">
