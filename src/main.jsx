3dea3edbe000b480f87aaf8cf9dc45029f8b9cf9
Suronjit797 <suronjit797@gmail.com>
2024-07-14 23:16:28 +0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import axios from "axios";
 
-
-
 // axios base url
 // axios.defaults.baseURL = "http://localhost:5000/api/v1";
 axios.defaults.baseURL = "https://question-server-jm21.onrender.com/api/v1";
@@ -15,9 +13,7 @@ axios.defaults.baseURL = "https://question-server-jm21.onrender.com/api/v1";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-
-        <App />
-
+      <App />
     </Provider>
   </React.StrictMode>
 );
